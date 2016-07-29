441d80a48a9bee741cc20fdd4f73cd7577aa2cb8
James Regnier <jgreg071@gmail.com>
2016-07-28 22:54:51 -0500

    Having fun with the CSS, all inner mechanics functional

---
+++ interactive.js
@@ -0,0 +1,17 @@
+// // DARK THEME //
+
+var darkTheme = document.getElementById("darkThemeBox")
+
+darkTheme.addEventListener("click", function(){
+    document.body.classList.toggle("darkTheme");
+});
+
+
+
+// // LARGE TEXT TOGGLE //
+
+var largeText = document.getElementById("largeTextBox")
+
+largeText.addEventListener("click", function(){
+    document.body.classList.toggle("largeText");
+});
--- messageGenerator.js
+++ messageGenerator.js
@@ -1,21 +1,3 @@
-// // DARK THEME //
-
-var darkTheme = document.getElementById("darkThemeBox")
-
-darkTheme.addEventListener("click", function(){
-    document.body.classList.toggle("darkTheme");
-});
-
-
-
-// // LARGE TEXT TOGGLE //
-
-var largeText = document.getElementById("largeTextBox")
-
-largeText.addEventListener("click", function(){
-    document.body.classList.toggle("largeText");
-});
-
 // Importing the default five messages from JSON document IIFE //
 var Chatty = (function (boardHandler) {
 
